d4e26fa987e9a736cbbba920c786721fbb6ba461
Ram <ramachandrannellai@gmail.com>
2018-12-03 21:38:49 +0530

    Initial setup

---
+++ tour-of-java8/src/main/java/org/learning/java8/pilot/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.learning.java8.pilot;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World..! Welcome to Java8 tour..");
+	}
+
+}
