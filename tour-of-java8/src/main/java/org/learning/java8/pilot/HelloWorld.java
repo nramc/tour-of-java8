50099fa4329a8e48cf14fc5107bbcd1634a79f6f
Ram <ramachandrannellai@gmail.com>
2018-12-03 22:04:51 +0530

    logging lib add

--- tour-of-java8/src/main/java/org/learning/java8/pilot/HelloWorld.java
+++ tour-of-java8/src/main/java/org/learning/java8/pilot/HelloWorld.java
@@ -1,9 +1,16 @@
 package org.learning.java8.pilot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld {
-	
+
+	private static Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello World..! Welcome to Java8 tour..");
+
+		LOGGER.info("Hello World..! Welcome to Java8 tour..");
+		
 	}
 
 }
