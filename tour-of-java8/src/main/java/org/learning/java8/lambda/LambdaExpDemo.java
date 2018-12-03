91d7ba262679c40fbfbc4d52b2c5dcbeb16e270c
Ram <ramachandrannellai@gmail.com>
2018-12-04 00:21:03 +0530

    lambda expression demo added

---
+++ tour-of-java8/src/main/java/org/learning/java8/lambda/LambdaExpDemo.java
@@ -0,0 +1,58 @@
+package org.learning.java8.lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LambdaExpDemo {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LambdaExpDemo.class);
+
+	public static void main(String[] args) {
+
+		List<String> listOfStr = new ArrayList<String>();
+		listOfStr.add("ONE");
+		listOfStr.add("TWO");
+		listOfStr.add("THREE");
+
+		/**
+		 * Passing instance of functional interface whic was created ion normal way without lambda as
+		 * argument to forEach method of List
+		 */
+		LOGGER.info("NORMAL WAY OF CREATING INSTANCE FOR FUNCTIONAL INTERFACE");
+		listOfStr.forEach(new Consumer<String>() {
+
+			public void accept(String str) {
+				LOGGER.info("{}", str);
+			}
+		});
+
+		/**
+		 * Create instance for functional interface by lambda and passing it as argument to forEach
+		 * method of List
+		 */
+		LOGGER.info("LAMBDA EXPRESSION | SINGLE LINE");
+		listOfStr.forEach(str -> LOGGER.info("{}", str));
+		
+		LOGGER.info("LAMBDA EXPRESSION | MULTI LINE");
+		listOfStr.forEach(str -> { LOGGER.info("{}", str); });
+		
+		/** Lambda expression with multiple parameters */
+		listOfStr.sort(new Comparator<String>() {
+
+			@Override
+			public int compare(String str1, String str2) {
+				return str1.compareToIgnoreCase(str2);
+			}});
+		listOfStr.forEach(str -> LOGGER.info("{}", str));
+		
+		LOGGER.info("LAMBDA EXPRESSION | MULTIPLE PARAMETERS");
+		listOfStr.sort( (str1, str2) -> str1.compareToIgnoreCase(str2) );
+		listOfStr.forEach(str -> LOGGER.info("{}", str));
+
+	}
+
+}
